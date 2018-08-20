05f48b1bcbb24dbf44ef4ea8e20a5920d1390d4e
terenitup <terentaniuchi@gmail.com>
2018-08-20 14:03:00 -0600

    built our frist class component for the inputs container, and built an input functional component to render custom inputs.

--- src/components/home.js
+++ src/components/home.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 
 import Header from './header';
+import Card from './card';
+// import Input from './input';
 
 class Home extends Component {
   render() {
     return (
       <div className='home'>
-        { Header() }        
+        { Header() } 
+        <Card />
       </div>
     );
   }
---
+++ src/components/input.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Input = (label) => {
+    return (
+        <div className="input">
+            <input />
+            <label>{ label }</label>
+        </div>
+    )
+}
+
+export default Input;
